dd0ea5d9f76894149fdd2a2471200f551a3d40d8
ZeProf2C <mail@normanalie.fr>
2020-10-28 21:55:11 +0400

    button change on click

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ font = tkFont.Font(size=60)
 def buttonPress(i, j):
     print("click %s %s" %(i, j))
     game.move(i, j)
+    button[i][j].config(text = game.getBoard(i, j).upper())
 
+button = [
+    [],
+    [],
+    []
+]
 
 for i in range(3):
     for j in range(3):
@@ -26,13 +32,15 @@ for i in range(3):
         )
         frame.grid(row=i, column=j, padx=5, pady=5)
 
-        button = tk.Button(
-            master=frame,
-            font=font,
-            text=game.getBoard(i, j),
-            command=lambda locI=i, locJ=j: buttonPress(locI, locJ),
-            width=3
+        button[i].append(
+            tk.Button(
+                master=frame,
+                font=font,
+                text=game.getBoard(i, j),
+                command=lambda locI=i, locJ=j: buttonPress(locI, locJ),
+                width=3
             )
-        button.pack(expand=1)
+        )
+        button[i][j].pack(expand=1)
 
 window.mainloop()
